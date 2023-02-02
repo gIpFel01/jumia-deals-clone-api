f5b33149a67ddffd8ec5fb86ebc27bf52f0bbea9
Soumahoro <shiroki.soum88@gmail.com>
2023-02-02 13:56:21 +0000

    first merge of the api

---
+++ src/main/java/com/jumiadealsclone/jumiadealscloneapi/JumiaDealsCloneApiApplication.java
@@ -0,0 +1,13 @@
+package com.jumiadealsclone.jumiadealscloneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumiaDealsCloneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumiaDealsCloneApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jumiadealsclone/jumiadealscloneapi/JumiaDealsCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jumiadealsclone.jumiadealscloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumiaDealsCloneApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
